71b2c24bcca2eea29fb8d6f7ffc854911c3bf087
luisborja82 <luisborja82@gmail.com>
2024-04-27 09:41:29 -0300

    Ejercitando

---
+++ TP2.py
@@ -0,0 +1,9 @@
+#print("Hola Mundo",3+2)
+
+# print('Esto es una prueba') 
+# print(10 - 1)
+
+numero1 = 10 
+numero2 = 5 
+resultado = numero1 * numero2 
+print('El producto entre ' + str(numero1) + ' y ' + str(numero2) + ' da ' + str(resultado)) 
---
+++ TP2_minombre.py
@@ -0,0 +1,9 @@
+# nombre= "Luis Alberto Borja"
+
+# print(nombre)
+
+nombre= "Luis" 
+apellido= "Borja"
+edad= 42
+
+print("Mi nombre completo es " + nombre +" " + apellido +" "+"y tengo "+ str(edad) + " años")
